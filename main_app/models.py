7a17ae26f44ff3d042dae1c7c1357049e74858de
raul-vital <raul.rsv@hotmail.com>
2025-01-28 19:31:39 -0800

    create Album model, install postgresql and create database

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+    title = models.CharField(max_length=100)
+    artist = models.CharField(max_length=100)
+    genres = models.CharField()
+    release_date = models.DateField()
+
+    def __str__(self):
+        return self.name
