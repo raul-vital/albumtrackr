022915b74a5f2540b6348a3c2cccec38020803aa
raul-vital <raul.rsv@hotmail.com>
2025-01-28 21:27:07 -0800

    add index.html template for single album, create detail route and view rendering

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('albums/', views.album_index, name='album-index')
+    path('albums/', views.album_index, name='album-index'),
+    path('albums/<int:album_id>/', views.album_detail, name='album-detail'),
 
 
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -8,3 +8,7 @@ def home(request):
 def album_index(request):
     albums = Album.objects.all()
     return render(request, 'albums/index.html', {'albums': albums})
+
+def album_detail(request, album_id):
+    album = Album.objects.get(id=album_id)
+    return render(request, 'albums/detail.html', {'album': album})
