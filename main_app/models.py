4c1ab1d012454f09bc0458811203f02b024652c9
Quan <qhoang2373@gmail.com>
2025-02-10 20:23:06 -0800

    add song model

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -15,3 +16,17 @@ class Album(models.Model):
 
     def get_absolute_url(self):
         return reverse('album-detail', kwargs={'album_id': self.id})
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    release_date = models.DateField(max_length=100)
+    release_country = models.CharField(max_length=100)
+    mood = models.CharField(max_length=100)
+    lyrics = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('song-detail', kwargs={'pk': self.id})
